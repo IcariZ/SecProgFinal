fa16e3ac90a019b738e76f0b4809a144ef4d6975
apanggkun <febryalvianjulianto@gmail.com>
2024-11-14 01:10:58 +0700

    Comment out social links section in the view

--- resources/views/components/social-links.blade.php
+++ resources/views/components/social-links.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-4">
+<!-- <div class="mt-4">
     <a class="inline-flex w-full items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-700 shadow-sm transition duration-150 ease-in-out hover:bg-gray-50 disabled:opacity-25"
         href="{{ route('socialite.auth', 'github') }}">
         GitHub
@@ -13,4 +13,4 @@
         href="{{ route('socialite.auth', 'facebook') }}">
         Facebook
     </a>
-</div>
+</div> -->
